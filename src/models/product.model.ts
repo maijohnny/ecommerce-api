bceda669efb8b8d87c25425819c1258d16e100c0
Zaw Win Tun <zawwintun@Zaws-MacBook-Air.local>
2026-03-11 10:47:07 +0630

    ecommerce-api

--- src/models/product.model.ts
+++ src/models/product.model.ts
@@ -13,7 +13,7 @@ const ProductSchema: Schema = new Schema({
     price: { type: Number, required: true, default: 0 },
     stock: { type: Number, required: true, default: 0 },
     categoryId: { type: Schema.Types.ObjectId, ref: 'Category', required: true },
-    barcode: { type: String, unique: true }
+    barcode: { type: String, unique: true, sparse: true}
 }, { timestamps: true });
 
 export default mongoose.model<IProduct>('Product', ProductSchema);
